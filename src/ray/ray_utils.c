87a81727c272a7ec3a896c38a514677d1a321fa9
Wenyan Lin <wennyan.lin@gmail.com>
2024-12-18 16:28:40 +0100

    [#22] Draw object's color

--- src/ray/ray_utils.c
+++ src/ray/ray_utils.c
@@ -6,7 +6,7 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 14:42:57 by wlin              #+#    #+#             */
-/*   Updated: 2024/12/05 16:42:28 by wlin             ###   ########.fr       */
+/*   Updated: 2024/12/16 18:14:25 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,30 @@ void	vec_copy(double vec_dest[3], double vec_src[3])
 	vec_dest[2] = vec_src[2];
 }
 
-int	color_converter(double color[3])
+int	color_converter(int color[3])
 {
-	int r = (int)(255.999 * color[0]);
-	int g = (int)(255.999 * color[1]);
-	int b = (int)(255.999 * color[2]);
+	return (color[0] << 16) | (color[1] << 8) | color[2];
+}
+
+int	normal_color(double normal[3])
+{
+	int r = (int)(255.999 * ((normal[0] + 1) / 2));
+	int g = (int)(255.999 * ((normal[1] + 1) / 2));
+	int b = (int)(255.999 * ((normal[2] + 1) / 2));
 	return (r << 16) | (g << 8) | b;
 }
 
-int ray_color(t_ray ray, t_obj sphere, double t) 
-{
-	double	result[3];
+// int ray_color(t_ray ray, t_obj sphere, double t) 
+// {
+// 	double	result[3];
 
-	if (t > 0)
-	{
-		ray_at_t(result, ray, t);
-		vec_sub(result, result, sphere.xyz);//result = nomal: a vector perpendicular to the hit point
-		vec_normalize(result);
-		vec_scale(result, (double [3]){result[0] + 1, result[1] + 1,
-			result[2] + 1}, 0.5);
-	}
-	return (color_converter(result));
-}
+// 	if (t > 0)
+// 	{
+// 		ray_at_t(result, ray, t);
+// 		vec_sub(result, result, sphere.xyz);//result = nomal: a vector perpendicular to the hit point
+// 		vec_normalize(result);
+// 		vec_scale(result, (double [3]){result[0] + 1, result[1] + 1,
+// 			result[2] + 1}, 0.5);
+// 	}
+// 	return (color_converter(result));
+// }
