a3504a1a3ad38d8eb3f2d229ee579939f68feb9e
Cléo Le Tron <cle-tron@car7s2.42barcelona.com>
2024-12-19 14:28:47 +0100

    [#17] norminette

--- src/light/lightning.c
+++ src/light/lightning.c
@@ -6,7 +6,7 @@
 /*   By: cle-tron <cle-tron@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 18:54:12 by cle-tron          #+#    #+#             */
-/*   Updated: 2024/12/18 19:06:31 by cle-tron         ###   ########.fr       */
+/*   Updated: 2024/12/19 14:28:17 by cle-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int	color_converter(int color[3], double intensity)
 {
-	int r = (int)(intensity * color[0]);
-	int g = (int)(intensity * color[1]);
-	int b = (int)(intensity * color[2]);
-	return (r << 16) | (g << 8) | b;
+	int	r;
+	int	g;
+	int	b;
+
+	r = (int)(intensity * color[0]);
+	g = (int)(intensity * color[1]);
+	b = (int)(intensity * color[2]);
+	return ((r << 16) | (g << 8) | b);
 }
 
 double	lambert_law(double light_dir[3], double norm[3])
@@ -32,17 +36,9 @@ double	lambert_law(double light_dir[3], double norm[3])
 
 void	lightning(t_hit_rec *rec, t_data *data)
 {
-
 	double	light_dir[3];
 
-//	vec_sub(point->norm, point->xyz, data->obj->xyz);
-//	vec_normalize(point->norm);
 	vec_sub(light_dir, data->light->xyz, rec->p);
 	vec_normalize(light_dir);
 	rec->intensity = lambert_law(light_dir, rec->normal) * data->light->ratio;
-
-
-//printf("x:%f, y:%f, z:%f\n", point->norm[0], point->norm[1], point->norm[0]);
-//printf("intensity: %f\n", point->intensity);
-
 }
